e1bc8f5579a5aa7536a2ecf1f2cb10b9eca5ecfa
Sviatoslav <sviatos1706@gmail.com>
2023-09-15 20:41:57 +0300

    add font size changer and font weight changer

---
+++ main.js
@@ -0,0 +1,49 @@
+let pageStyle={
+    fontSize:16,
+    weight:false,
+    cursyv:false
+}
+
+
+$('.fontSizeChanger').change(function(){
+    pageStyle.fontSize=$('#fontSizeChanger').val()
+    $('.page').css('font-size',`${pageStyle.fontSize}px`)
+})
+
+$('#weightChanger').mouseenter(function(){
+    $('#weightChanger').css('color','orange')
+    $('.page').css('font-weight','700')
+})
+$('#weightChanger').mouseleave(function(){
+    if(pageStyle.weight==false){
+        $('#weightChanger').css('color','#000')
+        $('.page').css('font-weight','normal')
+    }
+ 
+});
+
+$('#weightChanger').click(function(){
+    if(pageStyle.weight==false){
+        $('#weightChanger').css('color','orange');
+        $('.page').css('font-weight,`700`')
+        pageStyle.weight=true;
+    }else if(pageStyle.weight==true){
+        $('#weightChanger').css('color','#000')
+        $('.page').css('font-weight',`normal`)
+        pageStyle.weight=false;
+    }
+})
+
+
+
+
+
+// $('#cursyvChanger').mouseenter(function(){
+//     $('#cursyvChanger').css('color','orange')
+//     $('.page').css('font-style','italic')
+// })
+
+// $('#cursyvChanger').mouseenter(function(){
+//     $('#weightChanger').css('color','orange')
+//     $('.page').css('font-weight','700')
+// })
