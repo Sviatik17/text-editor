efb102cffc561fc5a8557cc5faf0294327a4be0b
Sviatoslav <sviatos1706@gmail.com>
2023-09-24 20:14:52 +0300

    add srossed and underline text

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let pageStyle={
     fontSize:16,
     weight:false,
-    cursyv:false
+    italic:false,
+    underline:false,
+    crossed:false
 }
 
 
@@ -38,12 +40,90 @@ $('#weightChanger').click(function(){
 
 
 
-// $('#cursyvChanger').mouseenter(function(){
-//     $('#cursyvChanger').css('color','orange')
-//     $('.page').css('font-style','italic')
-// })
+$('#cursyvChanger').mouseenter(function(){
+    $('#cursyvChanger').css('color','orange')
+    $('.page').css('font-style','italic')
+})
+$('#cursyvChanger').mouseleave(function(){
+    if(pageStyle.italic==false){
+        $('#cursyvChanger').css('color','#000')
+        $('.page').css('font-style','normal')
+    }
+ 
+});
 
-// $('#cursyvChanger').mouseenter(function(){
-//     $('#weightChanger').css('color','orange')
-//     $('.page').css('font-weight','700')
-// })
+$('#cursyvChanger').click(function(){
+    if(pageStyle.italic==false){
+        $('#cursyvChanger').css('color','orange');
+        $('.page').css('font-style,`italic`')
+        pageStyle.italic=true;
+    }else if(pageStyle.italic==true){
+        $('#cursyvChanger').css('color','#000')
+        $('.page').css('font-style',`normal`)
+        pageStyle.italic=false;
+    }
+})
+
+
+
+
+$('#underlineChanger').mouseenter(function(){
+    $('#underlineChanger').css('color','orange')
+    $('.page').css('text-decoration','underline')
+})
+$('#underlineChanger').mouseleave(function(){
+    if(pageStyle.underline==false){
+        $('#underlineChanger').css('color','#000')
+        $('.page').css('text-decoration','none')
+    }
+ 
+});
+
+$('#underlineChanger').click(function(){
+    if(pageStyle.underline==false){
+        $('#underlineChanger').css('color','orange');
+        $('.page').css('text-decoration,`underline`')
+        pageStyle.underline=true;
+        pageStyle.crossed=true;
+    }else if(pageStyle.underline==true){
+        $('#underlineChanger').css('color','#000')
+        $('.page').css('text-decoration',`none`)
+        pageStyle.underline=false;
+        pageStyle.crossed=false;
+    }
+})
+
+
+
+
+
+$('#crossedChanger').mouseenter(function(){
+    $('#crossedChanger').css('color','orange')
+    $('.page').css('text-decoration','line-through')
+})
+$('#crossedChanger').mouseleave(function(){
+    if(pageStyle.crossed==false){
+        $('#crossedChanger').css('color','#000')
+        $('.page').css('text-decoration','none')
+    }
+ 
+});
+
+$('#crossedChanger').click(function(){
+    if(pageStyle.crossed==false){
+        $('#crossedChanger').css('color','orange');
+        $('.page').css('text-decoration,`line-through `')
+        pageStyle.underline=true;
+        pageStyle.crossed=true;
+    }else if(pageStyle.crossed==true){
+        $('#crossedChanger').css('color','#000')
+        $('.page').css('text-decoration',`none`)
+        pageStyle.underline=false;
+        pageStyle.crossed=false;
+    }
+})
+
+$('#colorChanger').change(function(){
+    // $('#page').css('color',`${colorChanger.val}`)
+    console.log(`${$('#colorChanger.val()')}`)
+})
